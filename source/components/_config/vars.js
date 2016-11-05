ccdc79289edd1c77b824fdadaa0ad7e7d88d3677
rodrigorozendobastos@gmail.com <rodrigorozendobastos@gmail.com>
2016-11-05 01:14:00 -0300

    Cria constante de permissão

--- source/components/_config/vars.js
+++ source/components/_config/vars.js
@@ -32,4 +32,51 @@ angular.module("app")
     "The password is invalid or the user does not have a password.":
       "Senha e Email inválidos"
   })
+
+  .factory('PermissionUtils', function (Ref, Auth, $q) {
+
+    /**
+     *
+     * @returns {*}
+     */
+    function requireAuth() {
+      return $q(function (resolve, reject) {
+        Auth.onAuthStateChanged(observer);
+        function observer(user) {
+          if (user) return resolve(user);
+          reject();
+        }
+      });
+    }
+
+    /**
+     * Retorna um a promessa com o papel do usuário
+     * @param auth
+     * @returns {*}
+     */
+    function getRole(auth) {
+      return $q(function (resolve, reject) {
+       Ref.child("roles").child(auth.uid).once("value")
+         .then( function (snap) {
+           if(!snap.val()) return reject();
+           resolve(snap.val() || 'donor');
+         });
+
+      });
+    }
+
+    return {
+      requireAuth: requireAuth,
+      getRole: getRole
+    };
+  })
+
+  .constant("PERMISSION", {
+    requireRole: function () {
+      return ['PermissionUtils', function (PermissionUtils) {
+        return PermissionUtils.requireAuth()
+          .then(PermissionUtils.getRole);
+      }];
+    }
+  })
 ;
