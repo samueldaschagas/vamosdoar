237256e9c7320ea46cbf7bf8786dd193ab551a90
rodrigorozendobastos@gmail.com <rodrigorozendobastos@gmail.com>
2017-03-05 03:04:35 -0300

    Organiza arquivos de configuração da aplicação
    de acordo com eslint

--- source/components/_config/vars.js
+++ source/components/_config/vars.js
@@ -1,39 +1,43 @@
-angular.module("app")
+angular.module('app')
 
-  .constant("ERRORCODES", {
-    "AUTH_REQURIRED": "Você não está mais autenticado",
-    "USER_AUTENTICADED": "Usuário autenticado",
-    "There is no user record corresponding to this identifier. The user may have been deleted.":
-      "Não há usuários com este email",
-    "The password is invalid or the user does not have a password.":
-      "Senha e Email inválidos"
+  // Mensagens de erros do firebase
+  .constant('ERRORCODES', {
+    AUTH_REQURIRED: 'Você não está mais autenticado',
+    USER_AUTENTICADED: 'Usuário autenticado',
+    'There is no user record corresponding to this identifier. The user may have been deleted.':
+      'Não há usuários com este email',
+    'The password is invalid or the user does not have a password.':
+      'Senha e Email inválidos'
   })
 
+  // Valida Permissão
   .factory('PermissionUtils', function (Ref, Auth, $q) {
-
+    /**
+     * Verifica se autenticação do usuário
+     * @returns {*}
+     */
     function requireAuth() {
       return $q(function (resolve, reject) {
-        Auth.onAuthStateChanged(observer);
         function observer(user) {
           if (user) return resolve(user);
-          reject();
+          return reject();
         }
+
+        Auth.onAuthStateChanged(observer);
       });
     }
 
     /**
-     * Retorna um a promessa com o papel do usuário
+     * Retorna uma promessa com o papel do usuário
      * @param auth
      * @returns {*}
      */
     function getRole(auth) {
       return $q(function (resolve, reject) {
-       Ref.child("roles").child(auth.uid).once("value")
-         .then( function (snap) {
-           if(!snap.val()) return reject();
-           resolve(snap.val() || 'donor');
-         });
-
+        Ref.child('roles').child(auth.uid).once('value', function (snap) {
+          if (!snap.val()) return reject();
+          return resolve(snap.val() || 'donor');
+        });
       });
     }
 
@@ -46,7 +50,8 @@ angular.module("app")
     };
   })
 
-  .constant("PERMISSION", {
+  // Permissão do usuário
+  .constant('PERMISSION', {
     requireRole: function () {
       return ['PermissionUtils', function (PermissionUtils) {
         return PermissionUtils.requireAuth()
@@ -56,7 +61,6 @@ angular.module("app")
     getCurrentUser: function () {
       return ['PermissionUtils', function (PermissionUtils) {
         return PermissionUtils.requireAuth();
-      }]
+      }];
     }
-  })
-;
+  });
