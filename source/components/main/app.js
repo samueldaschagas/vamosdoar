2fca87afa93cb94bc971f75823dc064ff4de6032
rodrigorozendobastos@gmail.com <rodrigorozendobastos@gmail.com>
2017-03-05 02:43:14 -0300

    Organiza controle de autenticação
    de acordo com eslint

--- source/components/main/app.js
+++ source/components/main/app.js
@@ -1,6 +1,6 @@
-angular.module("app", [
-  "ui.router",
-  "ngMaterial",
-  "ngMessages",
-  "ui.utils.masks"
+angular.module('app', [
+  'ui.router',
+  'ngMaterial',
+  'ngMessages',
+  'ui.utils.masks'
 ]);
